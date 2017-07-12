2ddd5bc169b2f04c95c15db71bbe2455fc53c3ed
Wilhelm Neubert <chillhelm@users.noreply.github.com>
2017-07-12 10:21:05 +0200

    Fixed a typo in the definition of the gradient of the solution

--- WN_Sheet09Ex06.m
+++ WN_Sheet09Ex06.m
@@ -1,9 +1,9 @@
 
 boundary_terms=@(x)x(1)^4*x(2)^5-17*sin(x(1)*x(2)); %this is the correct solution as well
 rhs_function=@(x) 20*x(1)^4*x(2)^3 + 12*x(1)^2*x(2)^5 + 17*x(1)^2*sin(x(1)*x(2)) + 17*x(2)^2*sin(x(1)*x(2)); % the laplace of the solution
-solgrad=@(x)[4*x(1)^3*x(2)^5-17*x(2)*cos(x(1)*x(2)) 5*x(1)*4*x(2)^4-17*x(1)*cos(x(1)*x(2)) ]; % gradient of the solution (needed for error computation
+solgrad=@(x)[4*x(1)^3*x(2)^5-17*x(2)*cos(x(1)*x(2)) 5*x(1)^4*x(2)^4-17*x(1)*cos(x(1)*x(2)) ]; % gradient of the solution (needed for error computation
 Errors=[];
-for n=2:4
+for n=2:8
     h=2^-n;
     %Generate a grid with the appropriate width
     [Nodes,Elements]=WN_GenerateGrid(h);
@@ -16,7 +16,7 @@ for n=2:4
     %Compute errors
     Errors = [Errors [n;h; WN_ComputeErrors(Elements,Nodes,uh,boundary_terms, solgrad)]];
     %Compute convergence rate
-    if n>2
+    if size(Errors,2)>1
       rate_L2 = (log(Errors(3,end))-log(Errors(3,end-1)))/(log(Errors(2,end))-log(Errors(2,end-1)));
       rate_H1 = (log(Errors(4,end))-log(Errors(4,end-1)))/(log(Errors(2,end))-log(Errors(2,end-1)));
     end
